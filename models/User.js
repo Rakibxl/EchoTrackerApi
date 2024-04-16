c434030db361520e44128cffdb65c2a19d71507b
void-zenith <zenithrajbhandari@gmail.com>
2024-04-16 12:10:26 -0400

    changed to live

--- models/User.js
+++ models/User.js
@@ -12,11 +12,6 @@ const UserSchema = new mongoose.Schema({
     type: mongoose.Schema.Types.ObjectId,
     ref: "Address",
   },
-  // streetName: { type: String, required: true },
-  // city: { type: String, required: true },
-  // postalCode: { type: String, required: true },
-  // province: { type: String, required: true },
-  // aptNumber: { type: String },
 });
 
 // Password hashing middleware, only if password is provided
